34588c1048b60b35a48fa97f4dd19fd68c1092f2
guyr18 <46636441+guyr18@users.noreply.github.com>
2020-03-06 23:45:55 -0500

    Add files via upload

---
+++ Assignment5/equiv.cpp
@@ -0,0 +1,20 @@
+// CSCI 2530
+// Assignment: 4
+// Author:     Robert Guy
+// File:       equiv.cpp
+// Tab stops:  4
+
+// This application provides an implementation
+// of a library that manages, combines, and
+// provides equivalency checks to equivalent
+// relations.
+
+
+#include equiv.h
+#include <cstdio>
+using namespace std;
+
+
+
+
+
